a30a5600479afac08314c8aad6dd744c3747f22d
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2005-11-03 20:04:52 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@19 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- dragon-sw-plus/run_daemon.sh
+++ dragon-sw-plus/run_daemon.sh
@@ -2,7 +2,7 @@
 #
 
 if test -f ./rce/schema_combo.rsd; then
-    SCHEMA_FILE=$SCHEMA_FILE
+    SCHEMA_FILE=./rce/schema_combo.rsd
 elif test -f /usr/local/etc/schema_combo.rsd; then
     SCHEMA_FILE=/usr/local/etc/schema_combo.rsd
 else
@@ -10,7 +10,11 @@ else
     exit 1
 fi 
 
-if ! test -f ./narb/narb.conf -o -f /usr/local/etc/narb.conf; then
+if test -f ./narb/narb.conf; then
+    NARB_CONF=./narb/narb.conf
+elif test -f /usr/local/etc/narb.conf; then
+    NARB_CONF=/usr/local/etc/narb.conf
+else
     echo 'dragon-sw-plus: NARB needs narb.conf!  -- refer to narb/narb.conf.sample...'
     exit 1
 fi      
@@ -18,12 +22,12 @@ fi
 ospf_intra_pid=`ps axwww | grep -v awk | awk '{if (match($0, "ospfd \-d \-f")) print $1}'`
 ospf_inter_pid=`ps axwww | grep -v awk | awk '{if (match($0, "ospfd \-d \-P 2614 \-I \-f")) print $1}'`
 
-if ! test "$ospf_intra_pid" = ""; then
-    echo 'WARNING! dragon-sw-plus: IntRA-domain OSPFd not running!'
+if test "$ospf_intra_pid" = ""; then
+    echo '****** WARNING: IntRA-domain OSPFd not running! ******'
 fi
 
-if ! test "$ospf_inter_pid" = ""; then
-    echo 'WARNING! dragon-sw-plus: IntER-domain OSPFd not running!'
+if test "$ospf_inter_pid" = ""; then
+    echo '****** WARNING: IntER-domain OSPFd not running! ******'
 fi
 
 if test -f ./rce/rce; then
@@ -33,17 +37,17 @@ elif test -f /usr/local/dragon/sbin/rce; then
     killall -9 rce
     /usr/local/dragon/sbin/rce -d -s $SCHEMA_FILE
 else
-    echo 'dragon-sw-plus: RCE executable does not exist...'
+    echo '******* ERROR: RCE executable does not exist *******'
     exit 1
 fi
 
 if test -f ./narb/narb; then
     killall -9 narb
-    ./narb/narb -d
+    ./narb/narb -d -f $NARB_CONF
 elif test -f /usr/local/dragon/sbin/narb; then
     killall -9 narb
-    /usr/local/dragon/sbin/narb -d
+    /usr/local/dragon/sbin/narb -d -f $NARB_CONF
 else
-    echo 'dragon-sw-plus: NARB executable does not exist...'
+    echo '******* ERROR: NARB executable does not exist *******'
     exit 1
 fi
