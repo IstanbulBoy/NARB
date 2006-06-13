5c61211f5dbee9104c295eee9f32585e4123afbd
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2006-06-13 22:37:10 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@80 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- dragon-sw-plus/narb/toolbox.hh
+++ dragon-sw-plus/narb/toolbox.hh
@@ -36,46 +36,6 @@ inline void htonf_mbps(u_int32_t &x)
     x = htonl(x);
 }
 
-enum G_PID {
-	G_Illegal = 0,
-	G_Reserved1 = 1,
-	G_Reserved2 = 2,
-	G_Reserved3 = 3,
-	G_Reserved4 = 4,
-	G_Asyn_E4 = 5,
-	G_Asyn_DS3 = 6,
-	G_Asyn_E3 = 7,
-	G_BitSyn_E3 = 8,
-	G_ByteSyn_E3 = 9,
-	G_Asyn_DS2 = 10,
-	G_BitSyn_DS2 = 11,
-	G_Reserved5 = 12,
-	G_Asyn_E1 = 13,
-	G_ByteSyn_E1 = 14,
-	G_ByteSyn_31DS0 = 15,
-	G_Asyn_DS1 = 16,
-	G_BitSyn_DS1 = 17,
-	G_ByteSyn_T1 = 18,
-	G_VC11 = 19,
-	G_Reserved6 = 20,
-	G_Reserved7 = 21,
-	G_DS1SFAsyn = 22,
-	G_DS1ESFAsyn = 23,
-	G_DS3M23Asyn = 24,
-	G_DS3CBitAsyn = 25,
-	G_VT_LOVC = 26,
-	G_STS_HOVC = 27,
-	G_POSUnscr16 = 28,
-	G_POSUnscr32 = 29,
-	G_POSScram16 = 30,
-	G_POSScram32 = 31,
-	G_ATM = 32,
-	G_Eth = 33,
-	G_SONET_SDH = 34,
-	G_Reserved = 35,
-	G_DigiWrapper = 36,
-	G_Lamda = 37
-};
 
 /* Structure for string<-->value conversion */
 struct string_value_conversion {
