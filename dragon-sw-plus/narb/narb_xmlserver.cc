953ba91b2123382dbdd48b4f6913f6555960a035
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2006-06-15 16:49:01 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@106 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- dragon-sw-plus/narb/narb_xmlserver.cc
+++ dragon-sw-plus/narb/narb_xmlserver.cc
@@ -365,7 +365,7 @@ int XML_LSP_Broker::ParseTopoQuery(xmlNodePtr cur)
                 xml_obuffer[0] = '\0';
                 if (xml_obuffer == NULL)
                     return ret;
-                xml_obufsize += sprintf(xml_obuffer, "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<narb version=\"1.0\">\n");
+                xml_obufsize += sprintf(xml_obuffer, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<narb version=\"1.0\">\n");
             }
             if (xml_obufsize >= (bufsize -XML_BUF_BLOCK_SIZE/2))
             {
@@ -393,6 +393,8 @@ int XML_LSP_Broker::ParseTopoQuery(xmlNodePtr cur)
             }
 
             xml_obufsize += sprintf(xml_obuffer+xml_obufsize, "</topology>\n");
+            xml_obufsize += sprintf(xml_obuffer+xml_obufsize, "</narb>");
+            xml_obuffer[xml_obufsize++] = '\0';
 
             if (xml_obufsize > 0)
             {
@@ -495,7 +497,7 @@ int XML_LSP_Broker::HandleReplyMessage (api_msg* rmsg)
         xml_obufsize = 0;
         if (xml_obuffer == NULL)
             return ret;
-        xml_obufsize += sprintf(xml_obuffer, "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<narb version=\"1.0\">\n");
+        xml_obufsize += sprintf(xml_obuffer, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<narb version=\"1.0\">\n");
     }
     
 
@@ -551,7 +553,7 @@ void XML_LSP_Broker::PrintXML_ERO (te_tlv_header* tlv_ero)
                 loose? "false":"true", ipbuf);
         else//Unnum
             xml_obufsize += sprintf(xml_obuffer+xml_obufsize, "<unnum loose=\"%s\" ip=\"%s\" ifid=\"%d\" />\n",
-                loose? "false":"true", ipbuf, 0x0400&subobj->l2sc_vlantag);
+                loose? "false":"true", ipbuf, 0x0400|subobj->l2sc_vlantag);
     }
     xml_obufsize += sprintf(xml_obuffer+xml_obufsize, "</ero>\n");
 }
