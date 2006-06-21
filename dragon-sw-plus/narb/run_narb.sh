8cd4427da37474f9b696193812da3e7da667346e
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2006-06-21 21:58:19 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@114 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- dragon-sw-plus/narb/run_narb.sh
+++ dragon-sw-plus/narb/run_narb.sh
@@ -5,6 +5,8 @@ if test -f ./rce/schema_combo.rsd; then
     SCHEMA_FILE=./rce/schema_combo.rsd
 elif test -f /usr/local/etc/schema_combo.rsd; then
     SCHEMA_FILE=/usr/local/etc/schema_combo.rsd
+elif test -f /usr/local/dragon/etc/schema_combo.rsd; then
+    SCHEMA_FILE=/usr/local/dragon/etc/schema_combo.rsd
 else
     echo 'dragon-sw-plus: RCE needs schema_combo.rsd!  -- refer to rce/schema_combo.rsd.sample...'
     exit 1
@@ -14,13 +16,15 @@ if test -f ./narb/narb.conf; then
     NARB_CONF=./narb/narb.conf
 elif test -f /usr/local/etc/narb.conf; then
     NARB_CONF=/usr/local/etc/narb.conf
+elif test -f /usr/local/dragon/etc/narb.conf; then
+    NARB_CONF=/usr/local/dragon/etc/narb.conf
 else
     echo 'dragon-sw-plus: NARB needs narb.conf!  -- refer to narb/narb.conf.sample...'
     exit 1
 fi      
 
-ospf_intra_pid=`ps axwww | grep -v awk | awk '{if (match($0, "ospfd \-d \-f")) print $1}'`
-ospf_inter_pid=`ps axwww | grep -v awk | awk '{if (match($0, "ospfd \-d \-P 2614 \-I \-f")) print $1}'`
+ospf_intra_pid=`ps axwww | grep -v awk | awk '{if (match($0, ".*/ospfd-intra")  || match($0, ".*/ospfd.*conf")) print $1}'`
+ospf_inter_pid=`ps axwww | grep -v awk | awk '{if (match($0, ".*/ospfd-inter")) print $1}'`
 
 if test "$ospf_intra_pid" = ""; then
     echo '****** WARNING: IntRA-domain OSPFd not running! ******'
