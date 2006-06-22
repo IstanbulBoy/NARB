fd25da4ba4175e251244b81abe78b322cc6b1b20
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2006-06-22 02:02:01 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@120 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- dragon-sw-plus/narb/run_narb.sh
+++ dragon-sw-plus/narb/run_narb.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 #
 
-if test -f ./rce/schema_combo.rsd; then
+PREFIX=/usr/local/dragon
+PREFIX2=/usr/local
+
+if test ! -f $PREFIX/bin/dragon.sh; then
+    echo "narb-sw-builder: $PREFIX/bin/dragon.sh does not exit. Install dragon-sw first."
+    exit 1
+fi
+
+if test -f $PREFIX/etc/schema_combo.rsd; then
+    SCHEMA_FILE=$PREFIX/etc/schema_combo.rsd
+elif test -f $PREFIX2/etc/schema_combo.rsd; then
+    SCHEMA_FILE=$PREFIX2/etc/schema_combo.rsd
+elif test -f ./rce/schema_combo.rsd; then
     SCHEMA_FILE=./rce/schema_combo.rsd
-elif test -f /usr/local/etc/schema_combo.rsd; then
-    SCHEMA_FILE=/usr/local/etc/schema_combo.rsd
-elif test -f /usr/local/dragon/etc/schema_combo.rsd; then
-    SCHEMA_FILE=/usr/local/dragon/etc/schema_combo.rsd
 else
-    echo 'dragon-sw-plus: RCE needs schema_combo.rsd!  -- refer to rce/schema_combo.rsd.sample...'
+    echo 'narb-sw: RCE needs schema_combo.rsd!  -- refer to rce/schema_combo.rsd.sample...'
     exit 1
 fi 
 
-if test -f ./narb/narb.conf; then
+if test -f $PREFIX/etc/narb.conf; then
+    NARB_CONF=$PREFIX/etc/narb.conf
+elif test -f $PREFIX2/etc/narb.conf; then
+    NARB_CONF=$PREFIX2/etc/narb.conf
+elif test -f ./narb/narb.conf; then
     NARB_CONF=./narb/narb.conf
-elif test -f /usr/local/etc/narb.conf; then
-    NARB_CONF=/usr/local/etc/narb.conf
-elif test -f /usr/local/dragon/etc/narb.conf; then
-    NARB_CONF=/usr/local/dragon/etc/narb.conf
 else
-    echo 'dragon-sw-plus: NARB needs narb.conf!  -- refer to narb/narb.conf.sample...'
+    echo 'narb-sw: NARB needs narb.conf!  -- refer to narb/narb.conf.sample...'
     exit 1
 fi      
 
@@ -34,27 +42,35 @@ if test "$ospf_inter_pid" = ""; then
     echo '****** WARNING: IntER-domain OSPFd not running! ******'
 fi
 
-if test -f ./rce/rce; then
+if test -f $PREFIX/sbin/rce; then
+    killall -9 rce
+    sleep 3
+    $PREFIX/sbin/rce -d -s $SCHEMA_FILE
+elif test -f $PREFIX2/sbin/rce; then
+    killall -9 rce
+    sleep 3
+    $PREFIX2/sbin/rce -d -s $SCHEMA_FILE
+elif test -f ./rce/rce; then
     killall -9 rce
     sleep 3
     ./rce/rce -d -s $SCHEMA_FILE
-elif test -f /usr/local/dragon/sbin/rce; then
-    killall -9 rce
-    sleep 3
-    /usr/local/dragon/sbin/rce -d -s $SCHEMA_FILE
 else
     echo '******* ERROR: RCE executable does not exist *******'
     exit 1
 fi
 
-if test -f ./narb/narb; then
+if test -f $PREFIX/sbin/narb; then
+    killall -9 narb
+    sleep 3
+    $PREFIX/sbin/narb -d -f $NARB_CONF
+elif test -f $PREFIX2/sbin/narb; then
+    killall -9 narb
+    sleep 3
+    $PREFIX2/sbin/narb -d -f $NARB_CONF
+elif test -f ./narb/narb; then
     killall -9 narb
     sleep 3
     ./narb/narb -d -f $NARB_CONF
-elif test -f /usr/local/dragon/sbin/narb; then
-    killall -9 narb
-    sleep 3
-    /usr/local/dragon/sbin/narb -d -f $NARB_CONF
 else
     echo '******* ERROR: NARB executable does not exist *******'
     exit 1
