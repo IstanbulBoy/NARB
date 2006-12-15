cc3502e521d7bfda8fa9353f22742e7200d6f33a
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2006-12-15 22:15:38 +0000

    *** empty log message ***
    git-svn-id: file:///var/www/svn/narb-sw/trunk@229 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/narb/zebra_ospfclient.cc
+++ narb-sw/narb/zebra_ospfclient.cc
@@ -1090,11 +1090,12 @@ te_tlv_header * ospf_te_link_subtlv_set_swcap_vlan(te_tlv_header * tlv_header, u
 //  }
     vlan_data_len -= 4; // the extra bytes after 8 bytes of layer specific info
     tlv_header_appended = (te_tlv_header*)malloc(tlv_size + vlan_data_len); 
-    memcpy((char*)tlv_header_appended, (char*)tlv_header, tlv_size + vlan_data_len);
+    memcpy((char*)tlv_header_appended, (char*)tlv_header, tlv_size);
     tlv_header_appended->length = htons(ntohs(tlv_header->length) + vlan_data_len);
     free(tlv_header);
     ifswcap_tlv = (te_link_subtlv_link_ifswcap*)((char*)tlv_header_appended + tlv_size - sub_tlv_size);
     ifswcap_tlv->header.length = htons(sub_tlv_size - TLV_HDR_SIZE + vlan_data_len);
+    memcpy (&ifswcap_tlv->link_ifswcap_data.ifswcap_specific_info.ifswcap_specific_vlan, &vlan_info, vlan_data_len + 8);
 
     return tlv_header_appended;
 }
