5b5e65802ef5d1ecd14167d699a25d07b4828b2a
alake <alake@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2007-06-29 16:10:26 +0000

    Updated TERCETest to output path
    git-svn-id: file:///var/www/svn/narb-sw/trunk@602 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- wsdragon/terce-ws/src/TERCETest.java
+++ wsdragon/terce-ws/src/TERCETest.java
@@ -1,32 +1,73 @@
 import java.rmi.RemoteException;
 
+import org.ogf.schema.nm.topology.ctrlplane._20070611.CtrlPlaneDomainContent;
+import org.ogf.schema.nm.topology.ctrlplane._20070611.CtrlPlaneLinkContent;
+import org.ogf.schema.nm.topology.ctrlplane._20070611.CtrlPlaneNodeContent;
+import org.ogf.schema.nm.topology.ctrlplane._20070611.CtrlPlanePortContent;
+
 import edu.internet2.hopi.dragon.terce.ws.types.rce.FindPath;
 import edu.internet2.hopi.dragon.terce.ws.types.rce.FindPathContent;
 import edu.internet2.hopi.dragon.terce.ws.types.rce.FindPathResponse;
 import edu.internet2.hopi.dragon.terce.ws.service.RCEFaultMessageException;
 import edu.internet2.hopi.dragon.terce.ws.service.TERCEStub;
 
+/**
+ * Test program for querying TERCE-WS accepts a source, destination, and TERCE location 
+ * then requests a path calculation
+ * 
+ * @author Andrew Lake(alake@internet2.edu)
+ *
+ */
 
 
 public class TERCETest {
 	public static void main(String[] args){
 		try {
-			TERCEStub terce = new TERCEStub("http://127.0.0.1:8080/axis2/services/TERCE");
+			TERCEStub terce = null;
 			FindPath fp = new FindPath();
 			FindPathContent request = new FindPathContent();
+			String src = null;
+			String dest = null;
 			
-			request.setSrcHost("207.75.164.204");
-			request.setDstHost("207.75.164.207");
+			/* Get arguments */
+			if(args.length == 3){
+				src = args[0];
+				dest = args[1];
+				terce = new TERCEStub(args[2]);
+			}else{
+				System.out.println("Expcted java TERCETest source-ip destination-ip terce-url");
+			}
+			
+			/* Format Request */
+			request.setSrcHost(src);
+			request.setDstHost(dest);
 			request.setBandwidth(100);
 			request.setPreferred(true);
 			request.setStrict(true);
 			request.setVtag("any");
 			request.setAllvtags(true);
 			
+			/* Send request and get response*/
 			fp.setFindPath(request);
 			FindPathResponse response;
 			response = terce.findPath(fp);
-			response.getFindPathResponse();
+			CtrlPlaneDomainContent[] domains = response.getFindPathResponse().getPath().getHops().getDomain();
+			
+			/* Output path in response */
+			System.out.println("Path: ");
+			for(CtrlPlaneDomainContent d : domains){
+				CtrlPlaneNodeContent n = d.getNode()[0];
+				CtrlPlanePortContent p = n.getNodePort()[0];
+				CtrlPlaneLinkContent l = p.getLink();
+				System.out.print(d.getId() + ":");
+				System.out.print(n.getId() + ":");
+				System.out.print(p.getId());
+				if(l != null){
+					System.out.print(":" + l.getId());
+				}
+				System.out.println();
+			}
+				
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
