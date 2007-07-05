0e3588f206dfbb98bb19f1adacb1840674fa8e8f
alake <alake@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2007-07-05 12:56:20 +0000

    Adding script that converts wsdl to types
    git-svn-id: file:///var/www/svn/narb-sw/trunk@622 aacf9c1e-ac80-435b-8e83-6affbfa3956d

---
+++ wsdragon/terce-ws/wsdl/wsdl2java.sh
@@ -0,0 +1,1 @@
+java org.apache.axis2.wsdl.WSDL2Java -o service -ss -sd -d adb -g -ns2p http://hopi.internet2.edu/DRAGON/TERCE/RCE=edu.internet2.hopi.dragon.terce.ws.types.rce,http://hopi.internet2.edu/DRAGON/TERCE/TEDB=edu.internet2.hopi.dragon.terce.ws.types.tedb  -p edu.internet2.hopi.dragon.terce.ws.service -uri TERCE.wsdl
