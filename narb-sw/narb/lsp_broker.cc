cbc5aabf8f050a554055f297bb81cd414b908d07
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2007-09-04 14:45:49 +0000

    Format change in LSP ERO display
    git-svn-id: file:///var/www/svn/narb-sw/trunk@723 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/narb/lsp_broker.cc
+++ narb-sw/narb/lsp_broker.cc
@@ -157,7 +157,7 @@ void LSPQ::DescribeLSPDetail(vector<string>& lsp_detail_v)
         inet_ntop(AF_INET, &subobj->addr, addr, 20);
         u_int32_t ifid = subobj->if_id;
         if (ifid == 0 && subobj->l2sc_vlantag != 0)
-            ifid = htonl((LOCAL_ID_TYPE_TAGGED_GROUP_GLOBAL << 16) | htons(subobj->l2sc_vlantag));
+            ifid = htonl((LOCAL_ID_TYPE_TAGGED_GROUP_GLOBAL << 16) | subobj->l2sc_vlantag);
         if (ifid == 0)
             sprintf(buf, " >> HOP-TYPE [%s]: %s [IPv4]", subobj->hop_type?"loose":"strict", addr);  
         else    
