7d96e044f8862be86f4533be323dbcf083f61887
jarda <jarda@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2007-09-04 03:04:29 +0000

    changes which, at the moment, affect nothing...
    git-svn-id: file:///var/www/svn/narb-sw/trunk@711 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- wsdragon/terce-ws/wsdl/wsdl2java.sh
+++ wsdragon/terce-ws/wsdl/wsdl2java.sh
@@ -1,1 +1,2 @@
-java org.apache.axis2.wsdl.WSDL2Java -o service -ss -sd -d adb -g -ns2p http://hopi.internet2.edu/DRAGON/TERCE/RCE=edu.internet2.hopi.dragon.terce.ws.types.rce,http://hopi.internet2.edu/DRAGON/TERCE/TEDB=edu.internet2.hopi.dragon.terce.ws.types.tedb  -p edu.internet2.hopi.dragon.terce.ws.service -uri TERCE.wsdl
+#!/bin/sh
+sh /usr/local/dragon/axis2-1.3-RC2/bin/wsdl2java.sh -ss -sd -d adb -g -ns2p http://hopi.internet2.edu/DRAGON/TERCE/RCE=edu.internet2.hopi.dragon.terce.ws.types.rce,http://hopi.internet2.edu/DRAGON/TERCE/TEDB=edu.internet2.hopi.dragon.terce.ws.types.tedb  -p edu.internet2.hopi.dragon.terce.ws.service -uri resources/TERCE.wsdl
