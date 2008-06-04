ddbcfd0d17a3ffd2972d723b152b8ab6b3f7f1c5
jarda <jarda@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-06-04 15:54:37 +0000

    decoupled ws request handlers server properties coupled more tightly with the server object (rather than the package)
    git-svn-id: file:///var/www/svn/narb-sw/trunk@993 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- wsdragon/terce/WS/Server.pm
+++ wsdragon/terce/WS/Server.pm
@@ -28,6 +28,7 @@ use warnings;
 use Socket;
 use GMPLS::Constants;
 use WS::External;
+use WS::Handlers;
 use IO::Socket::INET;
 use SOAP::Lite;
 use SOAP::Transport::HTTP;
@@ -35,7 +36,7 @@ use SOAP::Transport::HTTP;
 BEGIN {
 	use Exporter   ();
 	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
-	$VERSION = sprintf "%d.%03d", q$Revision: 1.29 $ =~ /(\d+)/g;
+	$VERSION = sprintf "%d.%03d", q$Revision: 1.30 $ =~ /(\d+)/g;
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw();
 	%EXPORT_TAGS = ();
@@ -43,30 +44,6 @@ BEGIN {
 }
 our @EXPORT_OK;
 
-use constant TERCE_TOPO_ID => "dcn.internet2.edu-1202827532";
-use constant TERCE_TOPO_XMLNS => "http://ogf.ogf/schema/network/topology/ctrlPlane/20071023";
-use constant TERCE_IDC_ID => "https://ndb3-blmt.abilene.ucaid.edu:8443/axis2/services/OSCARS";
-use constant TERCE_ID_PREF => "urn:ogf:network";
-use constant TERCE_DOMAIN_ID => "domain=dcn.internet2.edu";
-use constant TERCE_NODE_ID => "node=%s";
-use constant TERCE_PORT_ID => "port=%s";
-use constant TERCE_LINK_ID => "link=%s";
-
-
-use constant CTRLP_NODE_T => "CtrlPlane:CtrlPlaneNodeContent";
-use constant CTRLP_PORT_T => "CtrlPlane:CtrlPlanePortContent";
-use constant CTRLP_LINK_T => "CtrlPlane:CtrlPlaneLinkContent";
-use constant CTRLP_SWCAP_T => "CtrlPlane:CtrlPlaneSwcapContent";
-
-use constant SCOPE_ALL => "all";
-use constant SCOPE_ABS => "abstract";
-use constant SCOPE_CRL => "control";
-use constant SCOPE_DAT => "data";
-
-use constant SCOPE_ABS_M => (1<<0);
-use constant SCOPE_CRL_M => (1<<1);
-use constant SCOPE_DAT_M => (1<<2);
-use constant SCOPE_MAX => 3;
 
 use constant STAT_LINK_ID => 		(1<<0);
 use constant STAT_LINK_TYPE => 		(1<<1);
@@ -85,258 +62,31 @@ use constant STAT_LINK_COMPLETE => 	(
 	STAT_LINK_MAX_BW | 
 	STAT_LINK_SWCAP);
 
-use constant MIN_RES_BW => 50000000;
-
-my $srvr = undef;
-my $lport = undef;
-my $tq1 = undef;
-my $tq2 = undef;
 
 sub tag($$$);
 
 sub new {
 	shift;
-	($lport, $tq1, $tq2)  = @_;
+	my ($lp, $tq1, $tq2)  = @_;
 	my $self = {
+		server => new SOAP::Transport::HTTP::Daemon(
+			LocalAddr => inet_ntoa(INADDR_ANY),
+			LocalPort => $lp,
+			ReuseAddr => 1,
+			Blocking => 0
+		)->dispatch_to('WS::Handlers'),
+		lport => $lp,
+		queue1 => $tq1,
+		queue2 => $tq2,
 		abstract_tedb => undef,
 		control_tedb => undef,
 		data_tedb => undef,
 		xml => undef
 	};
 	bless $self;
-	$srvr = new SOAP::Transport::HTTP::Daemon(
-		LocalAddr => inet_ntoa(INADDR_ANY),
-		LocalPort => $lport,
-		ReuseAddr => 1,
-		Blocking => 0
-		)->dispatch_to($self);
 	return $self;
 }
 
-############################## WS API ##############################
-sub findPath {
-	my $self = shift;
-	my($src, $dst, $bw, $enc, $swcap, $gpid, $vtag) = @_;
-	if(!(defined($src) && 
-			defined($dst) &&
-			defined($bw) &&
-			defined($enc) &&
-			defined($swcap) &&
-			defined($gpid) &&
-			defined($vtag))) {
-		$self->generate_soap_fault('Sender', 
-			'incomplete path request', 
-			'TerceRCEFault', 
-			'findPath must be fully specified');
-	}
-
-	
-	Aux::print_dbg_ws("findPath()\n");
-}
-
-sub selectNetworkTopology {
-	my $self = shift;
-	my($scope) = @_;
-	if(!defined($scope)) {
-		$self->generate_soap_fault('Sender', 
-			'no topology set specified', 
-			'TerceTedbFault', 
-			'<scope> must be specified');
-	}
-	elsif(
-		lc($scope) ne SCOPE_ALL && 
-		lc($scope) ne SCOPE_ABS && 
-		lc($scope) ne SCOPE_CRL && 
-		lc($scope) ne SCOPE_DAT ) {
-		$self->generate_soap_fault('Sender', 
-			'unknown topology type', 
-			'TerceTedbFault', 
-			'$scope not defined in the service description');
-	}
-	else {
-		my $scope_m = 0;
-		$scope_m |= (lc($scope) eq "abstract")?SCOPE_ABS_M:0;
-		$scope_m |= (lc($scope) eq "control")?SCOPE_CRL_M:0;
-		$scope_m |= (lc($scope) eq "data")?SCOPE_DAT_M:0;
-		$scope_m |= (lc($scope) eq "all")?(SCOPE_CRL_M | SCOPE_DAT_M | SCOPE_ABS_M):0;
-		Aux::print_dbg_ws("selectNetworkTopology($scope)\n");
-		$self->generate_soap_resp($scope_m);
-	}
-	return $$self{xml}; 
-}
-
-sub insertNetworkTopology {
-	my $self = shift;
-	Aux::print_dbg_ws("insertNetworkTopology()\n");
-	$self->generate_soap_fault('Receiver', 
-		'method not implemented', 
-		'TerceTedbFault', 
-		'insertNetworkTopology is not implemented');
-}
-
-####################################################################
-##########################  XML Formaters ##########################
-####################################################################
-
-sub get_swcap_xml($) {
-	my $self = shift;
-	my ($lr) = @_;
-	my $enc_xml = SOAP::Data->name('encodingType' => $link_swcap_enc_xml{$$lr{sw_cap}{enc}});
-	my $swtype_xml = SOAP::Data->name('switchingcapType' => $link_swcap_cap_xml{$$lr{sw_cap}{cap}});
-	my $info_xml = undef;
-	my @swcap = ();
-	if($$lr{sw_cap}{enc} == LINK_IFSWCAP_SUBTLV_ENC_ETH) {
-		if(exists($$lr{sw_cap}{vtags})) {
-			my $vtags = "";
-			my @vtags_xml = ();
-			for(my $i=0; $i<@{$$lr{sw_cap}{vtags}}; $i++) {
-				$vtags .= $$lr{sw_cap}{vtags}[$i];
-				if($i<(@{$$lr{sw_cap}{vtags}}-1)) {
-					$vtags .= ",";
-				}
-			}
-			push(@vtags_xml, SOAP::Data->name('vlanRangeAvailability' => $vtags));
-			push(@vtags_xml, SOAP::Data->name('interfaceMTU' => "9000")->type("xsd:string"));
-			$info_xml = SOAP::Data->name('switchingCapabilitySpecficInfo' => \SOAP::Data->value(@vtags_xml));
-		}
-	}
-	else {
-		$info_xml = SOAP::Data->name('switchingCapabilitySpecficInfo' => 
-			\SOAP::Data->value(SOAP::Data->name('capability' => 'unimplemented')));
-	}
-	push(@swcap, $enc_xml, $swtype_xml);
-	push(@swcap, $info_xml) if defined($info_xml);
-	return SOAP::Data->name('SwitchingCapabilityDescriptors' => \SOAP::Data->value(@swcap));
-}
-
-sub get_nodes_xml($) {
-	my $self = shift;
-	my ($db) = @_;
-	my @nodes_xml = ();
-	foreach my $rtr (keys %$db) {
-		my $rn = WS::External::get_rtr_name($rtr);
-		my $rtr_id = sprintf(TERCE_NODE_ID, defined($rn)?$rn:sprintf("%08x", $rtr));
-		my @ports_xml = ();
-		foreach my $link (keys %{$$db{$rtr}}) {
-			# skip the top-level descriptors
-			if(ref($$db{$rtr}{$link}) ne "HASH") {
-				next;
-			}
-			#port
-			my $pn = WS::External::get_port_name($rtr, $link);
-			$pn = exists($$db{$rtr}{$link}{remote})?
-			("DTL".(defined($pn)?$pn:sprintf("%08x", $link))):
-			("S".(defined($pn)?$pn:sprintf("%08x", $link)));
-			#check for uni info
-			if(exists($$db{$rtr}{$link}{sw_cap}{uni})) {
-				if(defined($$db{$rtr}{$link}{sw_cap}{uni}{id})) {
-					$pn = "S".sprintf("%d", ($$db{$rtr}{$link}{sw_cap}{uni}{id}<<8) | 255);
-				}
-			}
-			my $p_id = sprintf(TERCE_PORT_ID, $pn);
-			my $fqp_id = TERCE_ID_PREF.":".TERCE_DOMAIN_ID.":".$rtr_id.":".$p_id;
-			#link
-			my $l_id = sprintf(TERCE_LINK_ID, inet_ntoa(pack("N", $$db{$rtr}{$link}{local})));
-			my $fql_id = TERCE_ID_PREF.":".TERCE_DOMAIN_ID.":".$rtr_id.":".$p_id.":".$l_id;
-			#swcap
-			my $swcap_xml = $self->get_swcap_xml($$db{$rtr}{$link});
-
-			#the rest
-			my $cap_xml = SOAP::Data->name('capacity' => $$db{$rtr}{$link}{capacity})->type("xsd:string");
-			my $maxrb_xml = SOAP::Data->name('maximumReservableCapacity' => $$db{$rtr}{$link}{max_bw})->type("xsd:string");
-			my $minrb_xml = SOAP::Data->name('minimumReservableCapacity' => MIN_RES_BW)->type("xsd:string");
-			my $gran_xml = SOAP::Data->name('granularity' => MIN_RES_BW)->type("xsd:string");
-			my $met_xml = SOAP::Data->name('trafficEngineeringMetric' => $$db{$rtr}{$link}{metric})->type("xsd:string");
-			my $rmt_xml;
-			if(exists($$db{$rtr}{$link}{remote})) {
-				my $rlink = $$db{$rtr}{$link}{remote};
-				my $rrn = WS::External::get_rtr_name($$rlink{rtr_id});
-				my $rrtr_id = sprintf(TERCE_NODE_ID, defined($rrn)?$rrn:sprintf("%08x", $$rlink{rtr_id}));
-				my $rpn = WS::External::get_port_name($$rlink{rtr_id}, $$rlink{link_id});
-				$rpn = "DTL".(defined($rpn)?$rpn:sprintf("%08x", $$rlink{link_id}));
-				my $rp_id = sprintf(TERCE_PORT_ID, $rpn);
-				my $fqp_id = TERCE_ID_PREF.":".TERCE_DOMAIN_ID.":".$rrtr_id.":".$rp_id;
-				#link
-				my $rl_id = sprintf(TERCE_LINK_ID, inet_ntoa(pack("N", $$rlink{local})));
-				my $rfql_id = TERCE_ID_PREF.":".TERCE_DOMAIN_ID.":".$rrtr_id.":".$rp_id.":".$rl_id;
-				$rmt_xml = SOAP::Data->name('remoteLinkId' => $rfql_id)->type("xsd:string");
-			}
-			else {
-				$rmt_xml = SOAP::Data->name('remoteLinkId' => 'urn:ogf:network:domain=*:node=*:port=*:link=*')->type("xsd:string");
-			}
-
-			my $link_xml = SOAP::Data->name('link' => \SOAP::Data->value($swcap_xml, $cap_xml, $maxrb_xml, $minrb_xml, $gran_xml, $met_xml, $rmt_xml))->type(CTRLP_LINK_T);
-			$link_xml->attr({ ' id' => $fql_id});
-			$cap_xml = SOAP::Data->name('capacity' => $$db{$rtr}{$link}{capacity})->type("xsd:string");
-			$maxrb_xml = SOAP::Data->name('maximumReservableCapacity' => $$db{$rtr}{$link}{max_bw})->type("xsd:string");
-			$minrb_xml = SOAP::Data->name('minimumReservableCapacity' => MIN_RES_BW)->type("xsd:string");
-			$gran_xml = SOAP::Data->name('granularity' => MIN_RES_BW)->type("xsd:string");
-			my $port_xml = SOAP::Data->name('port' => \SOAP::Data->value($link_xml, $cap_xml, $maxrb_xml, $minrb_xml, $gran_xml))->type(CTRLP_PORT_T);
-			$port_xml->attr({ ' id' => $fqp_id});
-			push(@ports_xml, $port_xml);
-		}
-		my $id = TERCE_ID_PREF.":".TERCE_DOMAIN_ID.":".$rtr_id;
-		my $addr_xml = SOAP::Data->name('address' => inet_ntoa(pack("N", $rtr)));
-		my $node_xml = SOAP::Data->name('node' => \SOAP::Data->value($addr_xml, @ports_xml))->type(CTRLP_NODE_T);
-		$node_xml->attr({ ' id' => $id});
-		push(@nodes_xml, $node_xml);
-	}
-	return @nodes_xml;
-}
-
-sub generate_soap_fault($$$$) {
-	my $self = shift;
-	die SOAP::Fault->faultcode($_[0])
-	->faultstring($_[1])
-	->faultdetail(SOAP::Data->name($_[2])
-		->value(\SOAP::Data->name('msg' => $_[3])));
-}
-
-sub generate_soap_resp($) {
-	my $self = shift;
-	my ($scope_m) = @_;
-	my $xml = {};
-	my $done = 0;
-
-	if(($scope_m & SCOPE_ABS_M) && !defined($$self{abstract_tedb})) {
-		$self->generate_soap_fault('Receiver', 
-			'abstract topology not ready', 
-			'TerceTedbFault', 
-			'TERCE has not received all the necessary information from narb/rce to form the response');
-	}
-	elsif(($scope_m & SCOPE_CRL_M) && !defined($$self{control_tedb})) {
-		$self->generate_soap_fault('Receiver', 
-			'control topology not ready', 
-			'TerceTedbFault', 
-			'TERCE has not received all the necessary information from narb/rce to form the response');
-	}
-	elsif(($scope_m & SCOPE_DAT_M) && !defined($$self{data_tedb})) {
-		$self->generate_soap_fault('Receiver', 
-			'data topology not ready', 
-			'TerceTedbFault', 
-			'TERCE has not received all the necessary information from narb/rce to form the response');
-	}
-	else {
-		my @dbs = ($$self{abstract_tedb}, $$self{control_tedb}, $$self{data_tedb});
-		for(my $i=0; $i<SCOPE_MAX; $i++) {
-			if(defined($dbs[$i]) && ($scope_m & (1<<$i))) {
-				my @nodes = ();
-				# <node> array of
-				push(@nodes, $self->get_nodes_xml($dbs[$i]));
-				# <idcID>
-				my $idcID_xml = SOAP::Data->name('idcId' => TERCE_IDC_ID);
-				# <domain>
-				my $domain_xml = SOAP::Data->name('domain' => \SOAP::Data->value(@nodes));
-				$domain_xml->attr({ ' id' => TERCE_ID_PREF.":".TERCE_DOMAIN_ID});
-
-				# construct the document
-				$xml = SOAP::Data->name('topology' => \SOAP::Data->value($idcID_xml, $domain_xml));
-				$xml->attr({ ' id' => TERCE_TOPO_ID, xmlns => TERCE_TOPO_XMLNS });
-				$$self{xml} = $xml;
-			}
-		}
-	}
-}
 
 ####################################################################
 
@@ -674,10 +424,10 @@ sub run() {
 	my @ret2 = ();
 	while(!$::ctrlC) {
 		# WS server
-		$srvr->handle;
+		$$self{server}->handle;
 
 		# rce or narb TEDB queue
-		$d = $tq1->dequeue_nb();
+		$d = $$self{queue1}->dequeue_nb();
 		if(defined($d)) {
 			my $res = $self->process_q($d, $tedb1, \$lblock1, \$tmp1, \$stat1);
 			if($res == 1) {
@@ -727,7 +477,7 @@ sub run() {
 			}
 		}
 		# rce or narb TEDB queue
-		$d = $tq2->dequeue_nb();
+		$d = $$self{queue2}->dequeue_nb();
 		if(defined($d)) {
 			my $res = $self->process_q($d, $tedb2, \$lblock2, \$tmp2, \$stat2);
 			if($res == 1) {
@@ -777,7 +527,7 @@ sub run() {
 			}
 		}
 	}
-	$srvr->shutdown(SHUT_RDWR);
+	$$self{server}->shutdown(SHUT_RDWR);
 	Aux::print_dbg_run("exiting web services thread\n");
 }
 
