49d8569f49dede2620fd19fd4aabc77b7d60ddb5
isiehpn <isiehpn@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-06-13 17:12:34 +0000

    Add support for Darwin to do_build.sh / do_install.sh.
    Only remaining issue with compiling narb-sw under Darwin is the sendfile()
    call in narb_test.cc.
    
    
    git-svn-id: file:///var/www/svn/narb-sw/trunk@1006 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/do_build.sh
+++ narb-sw/do_build.sh
@@ -100,6 +100,49 @@ case `uname` in
     echo '' && \
         echo "Now, as root, run 'sh do_install.sh' to complete the installation."
     ;;
+  *Darwin)
+    echo This is Darwin
+
+    echo '' && \
+	echo 'configuring rce...'
+    cd rce
+    ./configure  --prefix=$PREFIX --enable-ext-attr CFLAG=-g CPPFLAG=-g
+    if test $? != 0; then
+	echo "narb-sw-builder: rce configure error!"
+	exit 1
+    fi
+
+    echo '' && \
+	echo 'making rce...'
+    make
+    if test $? != 0; then
+	echo "narb-sw-builder: rce make error!"
+	exit 1
+    fi
+
+    echo '' && \
+	echo 'configuring narb...'
+    cd ../narb
+    ./configure  --prefix=$PREFIX CFLAG=-g CPPFLAG=-g
+    if test $? != 0; then
+	echo "narb-sw-builder: narb configure error!"
+	exit 1
+    fi
+
+    echo '' && \
+	echo 'making narb...'
+    make
+    if test $? != 0; then
+	echo "narb-sw-builder: narb make error!"
+	exit 1
+    fi
+
+    echo '' && \
+	echo 'narb-sw-builder build finished.'
+
+    echo '' && \
+        echo "Now, as root, run 'sh do_install.sh' to complete the installation."
+    ;;
   *)
     echo Do not know what kind of system this is, do it by hand
     ;;
--- narb-sw/do_install.sh
+++ narb-sw/do_install.sh
@@ -65,6 +65,30 @@ case `uname` in
     echo '' && \
      echo "narb-sw-installer install finished."
     ;;
+  *Darwin)
+    echo This is Darwin
+
+    echo '' && \
+        echo "installing rce..."
+    cd rce
+    make install
+    if test $? != 0; then
+	echo "narb-sw-installer: rce make install error!"
+	exit 1
+    fi
+
+    echo '' && \
+        echo "installing narb..."
+    cd ../narb
+    make install
+    if test $? != 0; then
+	echo "narb-sw-installer: narb make install error!"
+	exit 1
+    fi
+
+    echo '' && \
+     echo "narb-sw-installer install finished."
+    ;;
   *)
     echo Do not know what kind of system this is, do it by hand
     ;;
