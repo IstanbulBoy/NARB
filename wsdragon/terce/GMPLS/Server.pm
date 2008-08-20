d68309f72792865c53f7602f4448fcd87fc70793
jarda <jarda@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-08-20 02:07:21 +0000

    combined all socket data (both IPC and binary) into one call handled by one select object
    git-svn-id: file:///var/www/svn/narb-sw/trunk@1129 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- wsdragon/terce/GMPLS/Server.pm
+++ wsdragon/terce/GMPLS/Server.pm
@@ -33,7 +33,7 @@ use IO::Select;
 BEGIN {
 	use Exporter   ();
 	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
-	$VERSION = sprintf "%d.%03d", q$Revision: 1.20 $ =~ /(\d+)/g;
+	$VERSION = sprintf "%d.%03d", q$Revision: 1.21 $ =~ /(\d+)/g;
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw();
 	%EXPORT_TAGS = ();
@@ -56,79 +56,118 @@ sub new {
 			"pool" => $$proc_val{pool}, # empty
 			"select" => new IO::Select($$proc_val{fh}), # select handle
 			"parser" => new XML::Parser(Style => "tree"), # incomming data parser
-			"processor" => \&Aux::receive_msgs, # data processor
+			"processor" => \&process_msg, # msg processor
 
 			# object descriptor:
 			"sock" => $sock,	# server socket
-			"gmpls_select" => new IO::Select($sock) # select handle
 		};
 	};
 	if($@) {
 		die "$$proc_val{name} instantiation failed: $@\n";
 	}
+	$$self{select}->add($sock);
 	bless $self;
 	return $self;
-}
-
-sub run() {
-	my $self = shift;
-	my %msg;
-	my $sn;
-	my $err;
-	while(!$::ctrlC) {
-		if(!$$self{sock}->connected()) {
-			Log::log "err", "client disconnect\n";
-			last;
-		}
-
-		eval {
-			$sn = GMPLS::API::get_msg($$self{gmpls_select}, \%msg);
-		};
-		if($@) {
-			Log::log "err", "$@\n";
-			last;
-		}
-		if(!defined($sn)) {
-			# timeout on select
-			next;
-		}
-		eval {
-			$err = 0;
-			if(GMPLS::API::is_sync_init($msg{$sn})) {
-				Log::log "info", "starting $$self{name}\n";
-				# init the control channel
-				my @data = ($$self{sock}->peerhost(), $msg{$sn}{hdr}{tag2});
-				unshift(@data, {"fmt"=>"", "cmd"=>CTRL_CMD, "type"=>INIT_Q_T});
-				Aux::send_msg($self, ADDR_GMPLS_CORE, @data);
-				GMPLS::API::ack_msg($$self{sock}, $msg{$sn});
-			}
-			elsif(GMPLS::API::is_sync_insert($msg{$sn})) {
-				if(GMPLS::API::parse_msg($msg{$sn}{data}, $self) <0) {
-					$err = 1;
-				}
-				GMPLS::API::ack_msg($$self{sock}, $msg{$sn}, $err);
-			}
-			elsif(GMPLS::API::is_delim($msg{$sn})) {
-				$self->activate_tedb();
-				$msg{$sn} = {};
-			}
-			else {
-				GMPLS::API::ack_msg($$self{sock}, $msg{$sn});
-			}
-		};
-		if($@) {
-			die "$@\n";
-		}
-		threads->yield();
-	}
-	Aux::print_dbg_run("exiting %s server thread\n", $$self{name});
-	close($$self{sock});
 }
 
 sub activate_tedb() {
 	my $self = shift;
 	my @cmd = ({"fmt"=>"", "cmd"=>TEDB_ACTIVATE});
 	Aux::send_msg($self, ADDR_GMPLS_CORE, @cmd);
+}
+
+sub process_msg() {
+	my $self = shift;
+	my ($msg)  = @_;
+	my $d;
+
+	# parse the message
+	my $tr;  # XML tree reference
+	eval {
+		$tr = $$self{parser}->parse($msg);
+		$d = Lib::xfrm_tree("msg", $$tr[1]);
+		if(!defined($d)) {
+			Log::log("warning", "IPC message parsing failed\n");
+			return;
+		}
+	};
+	if($@) {
+		Log::log("err", "$@\n");
+		return;
+	}
+	if(defined($d)) {
+	}
+}
+
+sub process_bin_msg($) {
+	my $self = shift;
+	my ($fh) = @_;
+	my %msg;
+	my $sn;
+	my $err;
+	eval {
+		$sn = GMPLS::API::get_msg($fh, \%msg);
+	};
+	if($@) {
+		die "$@\n";
+	}
+	eval {
+		$err = 0;
+		if(GMPLS::API::is_sync_init($msg{$sn})) {
+			# init the control channel
+			my @data = ($$self{sock}->peerhost(), $msg{$sn}{hdr}{tag2});
+			unshift(@data, {"fmt"=>"", "cmd"=>CTRL_CMD, "type"=>INIT_Q_T});
+			Aux::send_msg($self, ADDR_GMPLS_CORE, @data);
+			GMPLS::API::ack_msg($$self{sock}, $msg{$sn});
+		}
+		elsif(GMPLS::API::is_sync_insert($msg{$sn})) {
+			if(GMPLS::API::parse_msg($msg{$sn}{data}, $self) <0) {
+				$err = 1;
+			}
+			GMPLS::API::ack_msg($$self{sock}, $msg{$sn}, $err);
+		}
+		elsif(GMPLS::API::is_delim($msg{$sn})) {
+			$self->activate_tedb();
+			$msg{$sn} = {};
+		}
+		else {
+			GMPLS::API::ack_msg($$self{sock}, $msg{$sn});
+		}
+	};
+	if($@) {
+		die "$@\n";
+	}
+}
+
+sub run() {
+	my $self = shift;
+	my %pipe_queue;
+	my $gmpls_fh;
+
+	Log::log "info", "starting $$self{name}\n";
+	while(!$::ctrlC) {
+		if(!$$self{sock}->connected()) {
+			Log::log "err", "client disconnect\n";
+			$$self{select}->remove($$self{sock});
+			last;
+		}
+		$gmpls_fh = Aux::act_on_msg($self, \%pipe_queue);
+		if(defined($gmpls_fh)) {
+			eval {
+				$self->process_bin_msg($gmpls_fh);
+			};
+			if($@) {
+				Log::log "err", "$@\n";
+				last;
+			}
+		}
+	}
+	if($$self{select}->exists($$self{sock})) {
+		$$self{select}->remove($$self{sock});
+	}
+	$$self{select}->remove($$self{fh});
+	Aux::print_dbg_run("exiting $$self{name}\n");
+	close($$self{sock});
 }
 
 1;
