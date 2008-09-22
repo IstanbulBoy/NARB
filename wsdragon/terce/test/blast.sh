09142c538c134dda8f665814b9a2059ad470e43f
jarda <jarda@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-09-22 21:00:44 +0000

    added blast.sh - a script to stress test terce, and a WS call to inject an external topology XML
    git-svn-id: file:///var/www/svn/narb-sw/trunk@1166 aacf9c1e-ac80-435b-8e83-6affbfa3956d

---
+++ wsdragon/terce/test/blast.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh abstract  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh abstract  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh abstract  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh abstract  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+ ./ws_client.sh data  &>/dev/null &
+ ./ws_client.sh control  &>/dev/null &
+
