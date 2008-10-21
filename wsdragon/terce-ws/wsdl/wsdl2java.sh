71be18ec14c6d3ecf8dfba6fd5ef01f52d86eb73
root <root@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-10-21 15:22:26 +0000

    delete unused branch/tag
    git-svn-id: file:///var/www/svn/narb-sw/trunk@1205 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- wsdragon/terce-ws/wsdl/wsdl2java.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sh /usr/local/dragon/axis2-1.3-RC2/bin/wsdl2java.sh -ss -sd -d adb -g -ns2p http://hopi.internet2.edu/DRAGON/TERCE/RCE=edu.internet2.hopi.dragon.terce.ws.types.rce,http://hopi.internet2.edu/DRAGON/TERCE/TEDB=edu.internet2.hopi.dragon.terce.ws.types.tedb  -p edu.internet2.hopi.dragon.terce.ws.service -uri resources/TERCE.wsdl
