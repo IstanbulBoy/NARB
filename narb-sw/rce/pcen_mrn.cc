93a4b465f37ae1f492efa56d3d65c8a59f420e7d
xyang <xyang@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2008-11-06 17:53:30 +0000

    Addition of base and interval attributes to represent arbitrary tags in ConstraintTagSet
    git-svn-id: file:///var/www/svn/narb-sw/trunk@1216 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/rce/pcen_mrn.cc
+++ narb-sw/rce/pcen_mrn.cc
@@ -1308,7 +1308,7 @@ int PCEN_MRN::PerformComputation()
         PCENLink *nextLink;
         PCENNode *nextNode;
         bool link_visited;
-        ConstraintTagSet nextWaveSet(MAX_WAVE_NUM);
+        ConstraintTagSet nextWaveSet(MAX_WAVE_NUM, 192000, 100);
         ConstraintTagSet nextVtagSet(MAX_VLAN_NUM);
         TSpec tspec_egress;
         tspec_egress.Update(switching_type_egress, encoding_type_egress, bandwidth_egress);
