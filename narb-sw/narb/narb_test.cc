2b46ba92cb1fe43b389e9896a53dceb498589c55
xyang <xyang@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2009-07-21 14:49:19 +0000

    minor bug fixes
    git-svn-id: file:///var/www/svn/narb-sw/branches/dynamic-optimization@1340 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/narb/narb_test.cc
+++ narb-sw/narb/narb_test.cc
@@ -448,7 +448,7 @@ int main(int argc, char* argv[])
     {
         int opt;
 
-        opt = getopt_long (argc, argv, "E:H:P:S:D:X:b:e:l:k:t:v:x:CLMOQUVZmas", longopts, 0);
+        opt = getopt_long (argc, argv, "E:H:P:R:S:D:X:b:e:l:k:t:v:x:CLMOQUVZmas", longopts, 0);
         if (opt == EOF)
             break;
 
