3a9a994933a433f8434dbcd85594cb2e95ff4c65
xyang <xyang@aacf9c1e-ac80-435b-8e83-6affbfa3956d>
2009-07-21 14:40:26 +0000

    git-svn-id: file:///var/www/svn/narb-sw/branches/dynamic-optimization@1339 aacf9c1e-ac80-435b-8e83-6affbfa3956d

--- narb-sw/rce/rce_config.cc
+++ narb-sw/rce/rce_config.cc
@@ -273,6 +273,8 @@ void SystemConfig::ConfigFromFile(ifstream& inFile)
                 SystemConfig::pce_algorithm = MRN_DCN;
             else if (strstr(blk_body, "mc-base") != NULL)
                 SystemConfig::pce_algorithm = MC_BASE;
+            else if (strstr(blk_body, "mc-sched") != NULL)
+                SystemConfig::pce_algorithm = MC_SCHEDULE;
             else if (strstr(blk_body, "spf") != NULL)
                 SystemConfig::pce_algorithm = SPF;
             else if (strstr(blk_body, "test") != NULL)
